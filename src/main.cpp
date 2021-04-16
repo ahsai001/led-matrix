fabec8c295d4843ba516efec6dbb8d65b6224fc4
Ahmad Saifullah <ahsai001@gmail.com>
2021-04-16 13:53:32 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,4 @@
 #include <Arduino.h>
-/*
-  Quick demo of major drawing operations on a single DMD
- */
 
 //required by DMD
 #include <SPI.h>
@@ -13,6 +10,14 @@
 #include <fonts/SystemFont5x7.h>
 #include <angka6x13.h>
 #include <Verdana10.h>
+#include <TimesNewRoman10.h>
+#include <SegoePrint10.h>
+#include <ShortStack10.h>
+#include <Distro12.h>
+#include <CourierNew10.h>
+#include <ArialBlack10.h>
+#include <angka.h>
+#include <angka_2.h>
 
 
 //define dimension of panel
@@ -22,20 +27,20 @@
 SoftDMD dmd(DISPLAY_COL_NUM,DISPLAY_ROW_NUM);
 
 DMD_TextBox clockBox(dmd, 0,0,15,16);
-DMD_TextBox clockBox2(dmd, 18,0,15,16);
+DMD_TextBox clockBox2(dmd, 17,0,15,16);
 DMD_TextBox infoBox(dmd, 32,0,32,8);
 DMD_TextBox infoBox2(dmd, 32,8,32,8);
 
 //clock without RTC
-int h = 22;
-int m = 48;
-int s = 0;
-const int hs = 2;
+int h = 23;
+int m = 59;
+int s = 30;
+const int hs = 2; 
 const int ms = 3;
 int state1 = 0;
 int state2 = 0;
 
-void centerInBox(SoftDMD &dmd, DMD_TextBox &box,uint32_t box_width, const char * text,const uint8_t *font){
+void print_center(SoftDMD &dmd, DMD_TextBox &box,uint32_t box_width, const char * text,const uint8_t *font){
   unsigned int length = dmd.stringWidth(text,font);
   float centerX = (box_width - length)/2;
   box.clear();
@@ -43,25 +48,35 @@ void centerInBox(SoftDMD &dmd, DMD_TextBox &box,uint32_t box_width, const char *
   box.print(text);
   box.scrollX(centerX);
 
-  Serial.print("kata : ");
-  Serial.println(text);
-  Serial.print("length : ");
-  Serial.println(length);
-  Serial.print("centerX : ");
-  Serial.println(centerX);
+  
+  print_debug("kata : ");
+  println_debug(text);
+  print_debug("length : ");
+  println_debug(length);
+  print_debug("centerX : ");
+  println_debug(centerX);
+  
 }
 
 
 void print_time(){
     dmd.selectFont(angka6x13);
+    clockBox.clear();
+    clockBox2.clear();
+    if(h < 10){
+      clockBox.print("0");
+    }
     clockBox.print(h);
+    if(m < 10){
+      clockBox2.print("0");
+    }
     clockBox2.print(m);
 }
 
 // the setup routine runs once when you press reset:
 void setup() {
   Serial.begin(9600);
-  dmd.setBrightness(30);
+  dmd.setBrightness(80);
   dmd.begin();
 }
 
@@ -75,8 +90,24 @@ void minutes_tuning(){
   m = m + 1;
 }
 
+void print_debug(const char * msg){
+  //Serial.print(msg);
+}
+
+void print_debug(int n, int base = 10){
+  //Serial.print(n, base);
+}
+
+void println_debug(const char * msg){
+  //Serial.println(msg);
+}
+
+void println_debug(int n, int base = 10){
+  //Serial.println(n, base);
+}
+
 // time var
-uint32_t t_now, t_clock_beat_last_updated, t_clock_tick_last_updated, t_jws_last_updated;
+uint32_t t_now, t_clock_beat_last_updated = 0, t_clock_tick_last_updated = 0, t_jws_last_updated = 0;
 const char * info2String[] = {"SUBUH",
                           "ZUHUR",
                           "ASHAR",
@@ -100,21 +131,28 @@ void loop() {
     {
       s = 0;
       m = m + 1;
-      print_time();
     }
     if (m == 60)
     {
       m = 0;
       h = h + 1;
-      print_time();
     }
     if (h == 24)
     {
       h = 0;
-      print_time();
     }
     
-
+    print_debug("jam :");
+    print_debug(h);
+    print_debug(" - ");
+    print_debug(m);
+    print_debug(" - ");
+    print_debug(s);
+    println_debug(" ");
+    
+    print_time();
+    
+    /*
     state1 = digitalRead(hs);
     if (state1 == 0)
     {
@@ -125,18 +163,21 @@ void loop() {
     {
       minutes_tuning();
     }
+    */
   }
 
   if(t_now - t_clock_beat_last_updated >= 500){
     t_clock_beat_last_updated = t_now;
-    dmd.drawFilledBox(15,5,16,6, GRAPHICS_XOR);
-    dmd.drawFilledBox(15,9,16,10, GRAPHICS_XOR);
+    dmd.drawFilledBox(14,5,15,6, GRAPHICS_XOR);
+    dmd.drawFilledBox(14,9,15,10, GRAPHICS_XOR);
+    println_debug("jam beat");
   }
 
-  if(t_now - t_jws_last_updated >= 3000){
+  if(t_now - t_jws_last_updated >= 5000){
     t_jws_last_updated = t_now;
-    centerInBox(dmd,infoBox, dmd.width/2,infoString[step], Verdana10);
-    centerInBox(dmd,infoBox2, dmd.width/2,info2String[step], SystemFont5x7);
+    println_debug("jws");
+    print_center(dmd,infoBox, dmd.width/2,infoString[step], angka_2);
+    print_center(dmd,infoBox2, dmd.width/2,info2String[step], System5x7);
     step++;
     if(step >= sizeof(infoString)/sizeof(infoString[0])){
       step = 0;
